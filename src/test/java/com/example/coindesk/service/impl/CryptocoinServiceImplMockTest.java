4d59f62036bd286b2239ef1d36a91e950347752c
Chris <becomerocker@gmail.com>
2022-06-29 13:06:12 +0800

    coindesk 單元測試實作 (Service層 Mockito done)

--- src/test/java/com/example/coindesk/service/impl/CryptocoinServiceImplMockTest.java
+++ src/test/java/com/example/coindesk/service/impl/CryptocoinServiceImplMockTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import java.util.Optional;
 
@@ -19,6 +20,11 @@ public class CryptocoinServiceImplMockTest {
     @Autowired
     private CryptocoinService cryptocoinService;
 
+    //一部分參照真實寫出的邏輯功能，一部分是可自行定義假的功能
+//  @SpyBean
+//  private CryptocoinDao cryptocoinDao;
+
+    //完全自行定義假的相關物件、參數及功能
     @MockBean
     private CryptocoinDao cryptocoinDao;
 
@@ -29,12 +35,20 @@ public class CryptocoinServiceImplMockTest {
         mockCoin.setName("Mockcoin");
         mockCoin.setCode("MOCK");
 
-        Mockito.when(cryptocoinDao.findById(2)).thenReturn(Optional.of(mockCoin));
+//        Mockito.when(cryptocoinDao.findById(2)).thenReturn(Optional.of(mockCoin));
 
-        Cryptocoin coin = cryptocoinService.getCoinById(2);
+        //在findById裡給任何id值都會回傳定義的假物件
+        Mockito.when(cryptocoinDao.findById(Mockito.any())).thenReturn(Optional.of(mockCoin));
+        //另一種寫法...
+        Mockito.doReturn(Optional.of(mockCoin)).when(cryptocoinDao.findById(Mockito.any()));
+
+
+        Cryptocoin coin = cryptocoinService.getCoinById(3);
 
         Optional<Cryptocoin> cryptocoin = Optional.ofNullable(coin);
+
         assertNotNull(cryptocoin);
+        assertTrue(cryptocoin.isPresent());
 
         Optional<Integer> id = Optional.ofNullable(coin.getId());
         assertEquals(10, id.get());
